1a375ad6f74955d9d845e81d1d7f20dc3027d043
Student <Student@DESKTOP-KGK4MVM>
2024-05-04 17:45:11 +0200

    Add project files.

---
+++ Projekt/Program.cs
@@ -0,0 +1,124 @@
+﻿using GLFW;
+using GlmSharp;
+
+using Shaders;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+using System.Drawing;
+
+namespace PMLabs
+{
+    //Implementacja interfejsu dostosowującego metodę biblioteki Glfw służącą do pozyskiwania adresów funkcji i procedur OpenGL do współpracy z OpenTK.
+    public class BC: IBindingsContext
+    {
+        public IntPtr GetProcAddress(string procName)
+        {
+            return Glfw.GetProcAddress(procName);
+        }
+    }
+
+    class Program
+    {
+
+        static ShaderProgram shader;
+        static float speed_y;
+        static float speed_x;
+
+        static KeyCallback kc = KeyProcessor;
+
+        public static void KeyProcessor(System.IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods) { 
+            
+        }
+
+        public static void InitOpenGLProgram(Window window)
+        {
+            GL.ClearColor(0, 0, 0, 1);
+            shader = new ShaderProgram("v_shader.glsl", "f_shader.glsl");
+            Glfw.SetKeyCallback(window, kc);
+            GL.Enable(EnableCap.DepthTest);            
+
+        }
+
+        public static void FreeOpenGLProgram(Window window)
+        {
+           
+        }
+
+        //MODYFIKACJA. Ta wersja funkcji pozwala łatwo wczytać teksturę do innej jednostki teksturującej - należy ją podać jako argument.
+        public static int ReadTexture(string filename, TextureUnit textureUnit = TextureUnit.Texture0)
+        {
+            var tex = GL.GenTexture();
+            GL.ActiveTexture(textureUnit);
+            GL.BindTexture(TextureTarget.Texture2D, tex);
+
+            Bitmap bitmap = new Bitmap(filename);
+            System.Drawing.Imaging.BitmapData data = bitmap.LockBits(
+              new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+              System.Drawing.Imaging.ImageLockMode.ReadOnly,
+              System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width,
+              data.Height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+             
+            bitmap.UnlockBits(data);
+            bitmap.Dispose();
+
+            GL.TexParameter(TextureTarget.Texture2D,
+              TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D,
+              TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+
+            return tex;
+        }
+
+        public static void DrawScene(Window window)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            mat4 P = mat4.Perspective(glm.Radians(50.0f), 1, 1, 50);
+            mat4 V = mat4.LookAt(new vec3(0, 0, -3), new vec3(0, 0, 0), new vec3(0, 1, 0));
+
+            shader.Use();
+            GL.UniformMatrix4(shader.U("P"), 1, false, P.Values1D);
+            GL.UniformMatrix4(shader.U("V"), 1, false, V.Values1D);
+
+            mat4 M = mat4.Identity;
+            GL.UniformMatrix4(shader.U("M"), 1, false, M.Values1D);
+
+            Glfw.SwapBuffers(window);
+        }
+
+        static void Main(string[] args)
+        {
+            Glfw.Init();//Zainicjuj bibliotekę GLFW
+
+            Window window = Glfw.CreateWindow(500, 500, "OpenGL", GLFW.Monitor.None, Window.None);
+
+            Glfw.MakeContextCurrent(window);
+            Glfw.SwapInterval(1);
+
+            GL.LoadBindings(new BC());
+
+            InitOpenGLProgram(window);
+
+            Glfw.Time = 0;
+
+            while (!Glfw.WindowShouldClose(window))
+            {
+                Glfw.Time = 0;
+                DrawScene(window);
+
+                Glfw.PollEvents();
+            }
+
+
+            FreeOpenGLProgram(window);
+
+            Glfw.Terminate();
+        }
+                    
+
+    }
+}
