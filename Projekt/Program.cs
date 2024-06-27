d1f7d176caf33980ba63aa66f90629da40906d4b
Daniel Pilich <dpilich16@gmail.com>
2024-06-27 19:16:54 +0200

    Add sphere model

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,17 +1,15 @@
 ﻿using GLFW;
 using GlmSharp;
-
-using Shaders;
-
+using Models;
 using OpenTK;
 using OpenTK.Graphics.OpenGL4;
-
+using Shaders;
 using System.Drawing;
 
 namespace PMLabs
 {
     //Implementacja interfejsu dostosowującego metodę biblioteki Glfw służącą do pozyskiwania adresów funkcji i procedur OpenGL do współpracy z OpenTK.
-    public class BC: IBindingsContext
+    public class BC : IBindingsContext
     {
         public IntPtr GetProcAddress(string procName)
         {
@@ -26,24 +24,40 @@ namespace PMLabs
         static float speed_y;
         static float speed_x;
 
+        static Sphere sphere = new Sphere();
+
         static KeyCallback kc = KeyProcessor;
 
-        public static void KeyProcessor(System.IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods) { 
-            
+        public static void KeyProcessor(System.IntPtr window, Keys key, int scanCode, InputState state, ModifierKeys mods)
+        {
+            if (state == InputState.Press)
+            {
+                if (key == Keys.Left) speed_y = -3.14f;
+                if (key == Keys.Right) speed_y = 3.14f;
+                if (key == Keys.Up) speed_x = -3.14f;
+                if (key == Keys.Down) speed_x = 3.14f;
+            }
+            if (state == InputState.Release)
+            {
+                if (key == Keys.Left) speed_y = 0;
+                if (key == Keys.Right) speed_y = 0;
+                if (key == Keys.Up) speed_x = 0;
+                if (key == Keys.Down) speed_x = 0;
+            }
         }
 
         public static void InitOpenGLProgram(Window window)
         {
             GL.ClearColor(0, 0, 0, 1);
-            shader = new ShaderProgram("v_shader.glsl", "f_shader.glsl");
+            shader = new ShaderProgram("vertex_shader.glsl", "fragment_shader.glsl");
             Glfw.SetKeyCallback(window, kc);
-            GL.Enable(EnableCap.DepthTest);            
+            GL.Enable(EnableCap.DepthTest);
 
         }
 
         public static void FreeOpenGLProgram(Window window)
         {
-           
+
         }
 
         //MODYFIKACJA. Ta wersja funkcji pozwala łatwo wczytać teksturę do innej jednostki teksturującej - należy ją podać jako argument.
@@ -61,7 +75,7 @@ namespace PMLabs
 
             GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width,
               data.Height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-             
+
             bitmap.UnlockBits(data);
             bitmap.Dispose();
 
@@ -73,7 +87,7 @@ namespace PMLabs
             return tex;
         }
 
-        public static void DrawScene(Window window)
+        public static void DrawScene(Window window, float angle_x, float angle_y)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
@@ -84,8 +98,31 @@ namespace PMLabs
             GL.UniformMatrix4(shader.U("P"), 1, false, P.Values1D);
             GL.UniformMatrix4(shader.U("V"), 1, false, V.Values1D);
 
-            mat4 M = mat4.Identity;
+            mat4 M = mat4.Rotate(angle_y, new vec3(0, 1, 0)) * mat4.Rotate(angle_x, new vec3(1, 0, 0));
             GL.UniformMatrix4(shader.U("M"), 1, false, M.Values1D);
+
+            GL.Uniform4(shader.U("color"), 0.1f, 0.1f, 0.9f, 1f);
+
+            GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
+            GL.EnableVertexAttribArray(2);
+
+            //Kod rysujący. 
+            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 0, sphere.vertices);  //Powiąż dane z tablicy vertices z atrybutem 0
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, sphere.normals); //Powiąż dane z tablicy normals (wektory normalne ścian) z atrybutem 1
+            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 0, sphere.texCoords); //Powiąż dane z tablicy texCoords z atrybutem 2
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, sphere.vertexCount); //Rysuj model
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+
+            //Wyłącz atrybuty o numerach 0-3.
+            GL.DisableVertexAttribArray(0);
+            GL.DisableVertexAttribArray(1);
+            GL.DisableVertexAttribArray(2);
+
 
             Glfw.SwapBuffers(window);
         }
@@ -105,20 +142,22 @@ namespace PMLabs
 
             Glfw.Time = 0;
 
+            float angle_x = 0;
+            float angle_y = 0;
+
             while (!Glfw.WindowShouldClose(window))
             {
+                angle_x += speed_x * (float)Glfw.Time; //Aktualizuj kat obrotu wokół osi X zgodnie z prędkością obrotu
+                angle_y += speed_y * (float)Glfw.Time; //Aktualizuj kat obrotu wokół osi Y zgodnie z prędkością obrotu
                 Glfw.Time = 0;
-                DrawScene(window);
+                DrawScene(window, angle_x, angle_y);
 
                 Glfw.PollEvents();
             }
-
 
             FreeOpenGLProgram(window);
 
             Glfw.Terminate();
         }
-                    
-
     }
 }
